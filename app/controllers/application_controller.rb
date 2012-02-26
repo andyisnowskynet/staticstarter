7c458fe9468dacfe92bb701f3ccfee9515471245
Andy Schmidt <andy@thisislabcoat.com>
2012-02-25 21:16:00 -0800

    add render 404 helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def render_404
+    render :template => "pages/404", :layout => 'application', :status => :not_found, :content_type => Mime::HTML
+  end
+  
 end
